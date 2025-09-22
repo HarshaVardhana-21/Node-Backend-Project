59a79dec9a48938e54f20dbaa051e81a4c4379dc
HarshaVardhana-21 <harsha.a@epiance.com>
2025-09-22 17:05:55 +0530

    Git introduction and commands

---
+++ Track-project/Day_2.js
@@ -0,0 +1,18 @@
+//* Git introduction
+
+// * Git commands
+    //? git init
+    //? git status
+    //? git add .
+    //? git commit -m "Initial commit"
+    //? git branch -M main
+
+    //? git remote add origin <your-repo-link>
+
+        // where to get repo-link:
+        // go to github -> new repository -> create new repository -> copy the link
+
+    //? git push -u origin main || git push
+
+//* Pull changes from main
+    //? git pull origin main
